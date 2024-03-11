67597f3408ffedea864b340a8af82e4b4034d132
Vadym <goloveychuk1@gmail.com>
2024-03-11 16:16:08 +0200

    control fs

--- fuse/control.go
+++ fuse/control.go
@@ -10,7 +10,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"sync"
 	"syscall"
 
 	"github.com/hanwen/go-fuse/v2/fs"
@@ -21,72 +20,14 @@ import (
 // slice in memory.
 type Control struct {
 	fs.Inode
-
-	mu sync.Mutex
-	// Data []byte
-	// Attr fuse.Attr
-}
-
-var _ = (fs.NodeOpener)((*Control)(nil))
-var _ = (fs.NodeReader)((*Control)(nil))
-var _ = (fs.NodeWriter)((*Control)(nil))
-var _ = (fs.NodeFlusher)((*Control)(nil))
-var _ = (fs.NodeGetattrer)((*Control)(nil))
-var _ = (fs.NodeSetattrer)((*Control)(nil))
-
-func (f *Control) Open(ctx context.Context, flags uint32) (fh fs.FileHandle, fuseFlags uint32, errno syscall.Errno) {
-	fmt.Println("open")
-	return nil, fuse.FOPEN_DIRECT_IO, fs.OK
-}
-
-func (f *Control) Write(ctx context.Context, fh fs.FileHandle, data []byte, off int64) (uint32, syscall.Errno) {
-	fmt.Println("write", data)
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	// end := int64(len(data)) + off
-	// if int64(len(f.Data)) < end {
-	// 	n := make([]byte, end)
-	// 	copy(n, f.Data)
-	// 	f.Data = n
-	// }
-
-	// copy(f.Data[off:off+int64(len(data))], data)
-
-	return uint32(len(data)), 0
-}
-
-func (f *Control) Setattr(ctx context.Context, fh fs.FileHandle, in *fuse.SetAttrIn, out *fuse.AttrOut) syscall.Errno {
-	fmt.Println("setattr")
-	return 0
-}
-
-func (f *Control) Getattr(ctx context.Context, fh fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	// out.Mode = 0755
-	out.Owner = fuse.Owner{Uid: uint32(os.Getuid()), Gid: uint32(os.Getgid())}
-	out.Size = 100
-	return fs.OK
-}
-
-func (f *Control) Flush(ctx context.Context, fh fs.FileHandle) syscall.Errno {
-	fmt.Println("flush")
-	return 0
 }
 
 type Data struct {
 	Pid int
 }
 
-func (f *Control) Read(ctx context.Context, fh fs.FileHandle, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
-	log.Println("called")
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	d := Data{Pid: os.Getpid()}
-	data, err := json.Marshal(d)
-	if err != nil {
-		log.Fatalf("json.Marshal: %v", err)
-	}
+func (f *controlFh) Read(ctx context.Context, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
+	data := f.data
 	end := int(off) + len(dest)
 	if end > len(data) {
 		end = len(data)
@@ -94,19 +35,49 @@ func (f *Control) Read(ctx context.Context, fh fs.FileHandle, dest []byte, off i
 	return fuse.ReadResultData(data[off:end]), fs.OK
 }
 
+var _ = (fs.FileReader)((*controlFh)(nil))
+var _ = (fs.FileGetattrer)((*controlFh)(nil))
+var _ = (fs.FileFlusher)((*controlFh)(nil))
+
+var _ = (fs.NodeOpener)((*Control)(nil))
+var _ = (fs.NodeGetattrer)((*Control)(nil))
+
+type controlFh struct {
+	data []byte
+}
+
+func (f *controlFh) Getattr(ctx context.Context, out *fuse.AttrOut) syscall.Errno {
+	out.Size = uint64(len(f.data))
+	return fs.OK
+}
+
+func (f *controlFh) Flush(ctx context.Context) syscall.Errno {
+	fmt.Println("flush")
+	return 0
+}
+
+func (f *Control) Open(ctx context.Context, flags uint32) (fh fs.FileHandle, fuseFlags uint32, errno syscall.Errno) {
+	if flags == syscall.O_RDONLY {
+		d := Data{Pid: os.Getpid()}
+		data, err := json.Marshal(d)
+		if err != nil {
+			log.Fatalf("json.Marshal: %v", err)
+		}
+		fh := &controlFh{data: data}
+		return fh, fuse.FOPEN_DIRECT_IO, fs.OK
+	}
+	return nil, fuse.FOPEN_DIRECT_IO, fs.OK
+}
+
+func (f *Control) Getattr(ctx context.Context, fh fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
+	return fs.OK
+}
+
 type ControlWrap struct {
 	fs.Inode
-
-	// Data []byte
-	// Attr fuse.Attr
 }
 
 func (f *ControlWrap) Getattr(ctx context.Context, fh fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
-	// f.mu.Lock()
-	// defer f.mu.Unlock()
-	// out.Mode = 0755
-	// out.Owner = fuse.Owner{Uid: uint32(os.Getuid()), Gid: uint32(os.Getgid())}
-	out.Size = 100
 	return fs.OK
 }
 
